40c8f41db24fadb125e8c6da27035a12e8c62342
Julian cortes <mmm808383@gmail.com>
2024-06-26 07:05:03 -0500

    tipado primitivo

---
+++ src/tipadoPrimitivo.ts
@@ -0,0 +1,23 @@
+let bookTile: string = 'TypeScript Basics';
+const libraryName: string = 'My Library';
+let numberOfBooks: number = 23;
+let isLibraryOpen: boolean = true;
+
+//Books
+
+let titulo: string = 'La granja del Borrego';
+let autor: string = 'Carlos Alberto Diaz';
+let genero: string = 'Educacion';
+let idioma: string = 'Español';
+let precio: number = 52000;
+let formato: string = 'Libro';
+let isbn: number = 9786287675100;
+let descripcion: string = '¡Bienvenid@s a la Granja del Borrego! En este libro conocerán la historia del granjero más divertido de todos: ¿sí saben quién es? ¡Nada más y nada menos que el hijo favorito de "La mamá de las recetas"! Además, descubrirán todas las herramientas que necesitan para vivir en una granja y los secretos más asombrosos de la naturaleza. Y no solo eso, también se convertirán en l@s mejores amig@s de las gallinas empoderadas, de la vaca Rosalía y del cerdo Lolo. ¿Están list@s para empezar a sembrar sus propios alimentos en casa? ¿A hacer su propio compost, a identificar las expresiones de los caballos (eso es importante, granjer@s), a reconocer si un huevo está viejo o fresco y a ser más amigables con el entorno que los rodea? No esperen más, ¡alisten sus botas, sus sombreros y únanse al movimiento granjero!';
+let estado: string = 'Nuevo';
+let ubicacion: string = 'Bogota';
+let fecha_publicacion: number = 2/12/2023;
+let editorial: string = 'ALTEA';
+let paginas: number = 188;
+let dimensiones: string = 'Ancho: 15cm - Alto: 23cm' ;
+let peso: string = '50grs';
+let disponible: boolean = true;
